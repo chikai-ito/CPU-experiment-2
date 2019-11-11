ca87f5fb9b7b9f5c2aa5eb770794bcecb2b0a118
kenji-tojo <kenjitojo@icloud.com>
2019-11-11 22:33:41 +0900

    compiler

--- ver1/compiler/min-caml/fib.s
+++ ver1/compiler/min-caml/fib.s
@@ -38,3 +38,30 @@ ble_else.22:
 	lw	%r26 %r2 4
 	add	%r2 %r1 %r1
 	retl
+min_caml_create_array :
+	mov	%r1 %r3
+	mov	%r27 %r1
+create_array_loop :
+	bne     %r3 %r0 create_array_cont
+	beq	%r3 %r0	create_array_exit
+	addi	%r27 %r27 4
+create_array_exit :
+	retl
+create_array_cont :
+	sw	%r27 %r2 0
+	addi 	%r0 %r25 1
+	sub	%r3 %r25 %r3
+	addi	%r27 4 %r27
+	j	create_array_loop
+min_caml_create_float_array :
+	mov	%r1 %r2
+	mov	%r27 %r1
+create_float_array_loop :
+	bne	%r2 %r0	create_float_array_cont
+	retl
+create_float_array_cont :
+	sw.s	%r27 %f0 0
+	addi	%r0 1 %r25
+	sub	%r2 %r25 %r2
+	addi	%r27 4 %r27
+	j	create_float_array_loop
