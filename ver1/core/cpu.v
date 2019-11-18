443488418f3f27d591cb69137d53ab1a8ba042fb
chikai.ito <chikai.ito37@gmail.com>
2019-11-19 00:02:05 +0900

    Bug in memory

--- ver1/core/cpu.v
+++ ver1/core/cpu.v
@@ -12,7 +12,7 @@ module top #(CLK_PER_HALF_BIT = 520) (
     output wire [31:0] pcout,
     output wire [3:0] errout,
     input wire [31:0] read_data,
-    output wire [18:0] memory_addr,
+    output wire [19:0] memory_addr,
     output wire [31:0] write_data,
     output reg [3:0] err_pc,
     output wire we);
@@ -52,6 +52,11 @@ module top #(CLK_PER_HALF_BIT = 520) (
     wire [3:0] f_exception;
     wire fequal_res;
     wire fless_res;
+    wire [31:0] immediate;
+    wire [31:0] minus_immediate;
+    
+    assign immediate = (now_inst[15:15] == 1'b1) ? (32'b11111111111111110000000000000000 + now_inst[15:0]) : (now_inst[15:0]);
+    assign minus_immediate = (now_inst[15:15] == 1'b1) ? (32'b11111111111111110000000000000001 + now_inst[15:0]) : (now_inst[15:0] - 1);
     
     fpu1 fpu11(
         now_inst[31:26],
@@ -128,7 +133,6 @@ module top #(CLK_PER_HALF_BIT = 520) (
     localparam bg 			= 6'b000110;
     localparam fbne			= 6'b000011;
     localparam fbg			= 6'b000111;
-    localparam movs			= 6'b010001;
     localparam sll 			= 6'b000000;
 
     localparam equal    	= 1'b1;
@@ -138,7 +142,7 @@ module top #(CLK_PER_HALF_BIT = 520) (
     
     
     assign we = (now_inst[31:26] == sw || now_inst[31:26] == sws) && (status == s_first);
-    assign memory_addr = register_int[now_inst[25:21]] + now_inst[15:0];
+    assign memory_addr = register_int[now_inst[25:21]] + immediate;
     assign write_data = (now_inst[31:26] == sw) ? register_int[now_inst[20:16]] : register_float[now_inst[20:16]];
     
 
@@ -255,33 +259,34 @@ module top #(CLK_PER_HALF_BIT = 520) (
         					
         			endcase
                 cop1:
-                    case (now_inst[25:21])
-                        f_others:
-                            register_float[now_inst[10:6]] <= f_result;
-                        f_mfc1:
-                            register_int[now_inst[20:16]] <= f_result;
-                        f_mtc1:
-                            register_float[now_inst[15:11]] <= f_result;
-                    endcase
+                    if (now_inst[5:0] == s_mov) begin
+                         register_float[now_inst[20:16]] <= register_float[now_inst[25:21]];
+                    end else if(now_inst[25:21] == f_others) begin
+                        register_float[now_inst[10:6]] <= f_result;
+                    end else if(now_inst[25:21] == f_mfc1) begin
+                        register_int[now_inst[20:16]] <= f_result;
+                    end else if(now_inst[25:21] == f_mtc1) begin
+                        register_float[now_inst[15:11]] <= f_result;
+                    end
         		addi:
-        			register_int[now_inst[20:16]] <= register_int[now_inst[25:21]] + now_inst[15:0];
+        			register_int[now_inst[20:16]] <= register_int[now_inst[25:21]] + immediate;
         		lw:
         			register_int[now_inst[20:16]] <= read_data;
         		ilw:
-        			register_int[now_inst[20:16]] <= inst[register_int[now_inst[25:21]] + now_inst[15:0]];
+        			register_int[now_inst[20:16]] <= inst[register_int[now_inst[25:21]] + immediate];
         		//sw:
         			/* do nothing */
         		lws:
         			register_float[now_inst[20:16]] <= read_data;
         		ilws:
-        			register_float[now_inst[20:16]] <= inst[register_int[now_inst[25:21]] + now_inst[15:0]];
+        			register_float[now_inst[20:16]] <= inst[register_int[now_inst[25:21]] + immediate];
         		//sws:
         			/* do nothing */
         		j:
         			pc <= now_inst[25:0];
         		beq:
         			if (register_int[now_inst[25:21]] == register_int[now_inst[20:16]]) begin
-        				pc <= pc + now_inst[15:2] - 1;
+        				pc <= pc + minus_immediate;
         			end
         		jal: begin
         			register_int[28] <= pc;
@@ -293,29 +298,27 @@ module top #(CLK_PER_HALF_BIT = 520) (
         		end
         		bne:
         			if(register_int[now_inst[25:21]] != register_int[now_inst[20:16]]) begin
-        				pc <= pc + now_inst[15:0] - 1;
+        				pc <= pc + minus_immediate;
         			end
         		bl:
         			if(register_int[now_inst[25:21]] < register_int[now_inst[20:16]]) begin
-        				pc <= pc + now_inst[15:0] - 1;
+        				pc <= pc + minus_immediate;
         			end
 
         		bg:
         			if(register_int[now_inst[25:21]] > register_int[now_inst[20:16]]) begin
-        				pc <= pc + now_inst[15:0] - 1;
+        				pc <= pc + minus_immediate;
         			end
         		fbne:
-        			if(!fequal_res) begin:
-        				pc <= pc + now_inst[15:0] - 1;
+        			if(!fequal_res) begin
+        				pc <= pc + minus_immediate;
         			end
         		fbg:
-        			if(fless_res) begin:
-        				pc <= pc + now_inst[15:0] - 1;
+        			if(fless_res) begin
+        				pc <= pc + minus_immediate;
         			end
-        		movs:
-        			register_float[now_inst[20:16]] <= register_float[now_inst[25:21]];
         		sll:
-        			register_int[15:11] <= register_int[20:16] >> register_int[10:6];
+        			register_int[now_inst[15:11]] <= register_int[now_inst[20:16]] >> register_int[now_inst[10:6]];
         		default:
         		    if (now_inst[31:26] != sw) begin
         			     err <= 4'b0011;
